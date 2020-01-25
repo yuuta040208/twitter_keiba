4ab9b89ce51604fb8041c596f4e34cc2cff737e8
evitch <yuuta040208@gmail.com>
2020-01-25 21:00:46 +0900

    レース画面をリファクタリング

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,12 +1,18 @@
 class RacesController < ApplicationController
   def index
-    @races = Race.includes(:forecasts).all.order(date: 'desc').order(:hold)
+    @races = Race.includes(:forecasts).
+        order(date: 'desc').
+        order(:hold)
   end
 
   def show
     @race = Race.find(params[:id])
-    @horses = @race.horses.order(:umaban)
-    @forecasts = @race.forecasts.includes(:user, :tweet).order('users.point DESC').page(params[:page])
+    @horses = @race.horses.
+        order(:umaban)
+    @forecasts = @race.forecasts.
+        includes(:user, :tweet).
+        order('users.point DESC').
+        page(params[:page])
 
     if params[:search].present?
       @forecasts = @forecasts.search(params[:search])
--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -3,4 +3,31 @@ module RacesHelper
     return str if str.length != 8
     str.dup.insert(4, '/').insert(7, '/')
   end
+
+  def race_number(text)
+    text.delete("^0-9").to_i
+  end
+
+  def horse_color(sum, number)
+    index = Settings[:index][sum][number]
+    background_color = Settings[:color][index]
+    text_color = background_color == 'black' || background_color == 'red' || background_color == 'blue' ? 'white' : 'black'
+
+    "background-color: #{background_color}; color: #{text_color};"
+  end
+
+  def horse_mark(forecast, horse_name)
+    case horse_name
+    when forecast.honmei then
+      '◎'
+    when forecast.taikou then
+      '◯'
+    when forecast.tanana then
+      '▲'
+    when forecast.renka then
+      '△'
+    else
+      ''
+    end
+  end
 end
