1777b992087705ccfd39d9faf7e8b4b83fa20631
evitch <yuuta040208@gmail.com>
2020-01-26 22:11:27 +0900

    回収率を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(point: 'desc').page(params[:page])
+    @users = User.order(point: 'desc').
+        includes(:forecasts).
+        page(params[:page])
 
     if params[:search].present?
       @users = @users.search(params[:search])
-    end
-
-    @users.each do |user|
-      sum = 0
-      count = 0
-
-      user.forecasts.includes(race: :result).each do |forecast|
-        race = forecast.race
-        result = race.result
-
-        if result.present?
-          if forecast.honmei == result.first_horse
-            sum += Horse.find_by(name: forecast.honmei).odds * 100
-          end
-
-          count += 100
-        end
-      end
-
-      user.sum = (sum / count * 100).to_i
     end
   end
 
