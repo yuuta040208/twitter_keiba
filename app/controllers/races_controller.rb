610165a198053964f0c1e2effcfdd60809fe492d
evitch <yuuta040208@gmail.com>
2020-02-09 17:47:50 +0900

    推し馬にnilが表示されてしまっていたバグを修正

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -22,9 +22,6 @@ class RacesController < ApplicationController
         order('users.tanshou DESC').
         page(params[:page])
 
-    @tanshou_horse = tanshou_horse
-    @fukushou_horse = fukushou_horse
-
     @my_forecasts = my_forecasts
   end
 
@@ -35,41 +32,22 @@ class RacesController < ApplicationController
     tanshou_forecasts = @race.forecasts.
         includes(:user, :tweet).
         where(user_id: User.tanshou_masters.pluck(:id)).
+        where('forecasts.honmei IS NOT NULL').
         order('users.tanshou DESC')
     tanshou_honmeis = tanshou_forecasts.pluck(:honmei)
 
-    if tanshou_honmeis.empty?
-      return []
-    end
-
-    # tanshou_counts = tanshou_honmeis.uniq.map {|tanshou_honmei| tanshou_honmeis.count(tanshou_honmei)}
-    # if tanshou_counts.count(tanshou_counts.max) != 1 || tanshou_counts.max <= 1
-    #   return nil
-    # end
-    #
-    # tanshou_honmeis.uniq[tanshou_counts.index(tanshou_counts.max)]
-    tanshou_honmeis
-    # tanshou_honmeis.uniq.map{|tanshou_honmei| {name: tanshou_honmei, count: tanshou_honmeis.count(tanshou_honmei)}}
+    tanshou_honmeis.present? ? tanshou_honmeis : []
   end
 
   def fukushou_horse
     fukushou_forecasts = @race.forecasts.
         includes(:user, :tweet).
         where(user_id: User.fukushou_masters.pluck(:id)).
+        where('forecasts.honmei IS NOT NULL').
         order('users.fukushou DESC')
     fukushou_honmeis = fukushou_forecasts.pluck(:honmei)
 
-    if fukushou_honmeis.empty?
-      return []
-    end
-
-    # fukushou_counts = fukushou_honmeis.uniq.map {|fukushou_honmei| fukushou_honmeis.count(fukushou_honmei)}
-    # if fukushou_counts.count(fukushou_counts.max) != 1 || fukushou_counts.max <= 1
-    #   return nil
-    # end
-    #
-    # fukushou_honmeis.uniq[fukushou_counts.index(fukushou_counts.max)]
-    fukushou_honmeis
+    fukushou_honmeis.present? ? fukushou_honmeis : []
   end
 
   def my_forecasts
@@ -77,9 +55,6 @@ class RacesController < ApplicationController
     forecasts = horses.uniq.map {|horse| {name: horse, count: horses.count(horse)}}
 
     # 一人しか予想していないものは確度が低いため除外する
-    forecasts = forecasts.reject {|forecast| forecast[:count] == 1}
-    forecasts = forecasts.sort {|a, b| a[:count] <=> b[:count]}.reverse
-
-    forecasts
+    forecasts.reject {|forecast| forecast[:count] == 1}.sort {|a, b| a[:count] <=> b[:count]}.reverse
   end
 end
